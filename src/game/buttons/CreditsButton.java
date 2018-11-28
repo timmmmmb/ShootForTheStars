47e187fa5f94edf08e590cc9a36c2559ed265ae1
Tim Frey <frey.tim@outlook.com>
2018-11-28 11:29:23 +0100

    added new buttons to be used in menus

---
+++ src/game/buttons/CreditsButton.java
@@ -0,0 +1,22 @@
+package game.buttons;
+
+import game.settings.Settings;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+public class CreditsButton extends Button {
+    public CreditsButton(){
+        Image exitImage = new Image("Images/Menu Screen/Creditst_buttons.png", Settings.height /5.0,Settings.height/5.0,true,false);
+        Image exitImageOnHover = new Image("Images/Menu Screen/Creditst_buttons_pressed.png",Settings.height/5.0,Settings.height/5.0,true,false);
+        this.setGraphic(new ImageView(exitImage));
+        this.addEventHandler(MouseEvent.MOUSE_ENTERED,
+                e -> this.setGraphic(new ImageView(exitImageOnHover)));
+        this.addEventHandler(MouseEvent.MOUSE_EXITED,
+                e -> this.setGraphic(new ImageView(exitImage)));
+        this.setStyle(Settings.menuStyle);
+        this.setHeight(exitImage.getHeight());
+        this.setWidth(exitImage.getWidth());
+    }
+}
